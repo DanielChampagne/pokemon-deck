cd6bd3173861d8b96a1e21612f916898510c63b8
Daniel Champagne <danchampagne@hotmail.com>
2020-05-20 10:50:50 -0400

    Style App

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -11,7 +11,10 @@ class Pokecard extends Component {
         return(
             <div className="Pokecard">
                 <h1 className="Pokecard-title"> {this.props.name}</h1>
-                <img src={imgSrc} />
+                <div className="Pokecard-image">
+                    <img src={imgSrc} />  
+                </div>
+                
                 <div className="Pokecard-data">Type:{this.props.type}</div>
                 <div className="Pokecard-data"> EXP:{this.props.exp}</div>
             </div>
